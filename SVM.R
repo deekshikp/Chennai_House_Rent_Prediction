e3026cc5efb18562a4886b2a803e3048d0b2ac24
Dhee <150270603+heydhee@users.noreply.github.com>
2024-07-03 06:32:21 +0530

    Initial Commit

--- SVM.R
+++ SVM.R
@@ -6,6 +6,7 @@
 df<-chennai
 tibble(df)
 
+#Libraries
 library(ggplot2)
 library(dplyr)
 library(e1071)
@@ -13,16 +14,15 @@ library(e1071)
 #Qplot
 qplot(Size, Rent, data=chennai, color=BHK)
 
+#Model
 df<- subset(chennai, select = c(BHK, Rent, Size))
 tibble(df)
 mymodel <-svm(BHK~.,data=df, kernel="radial")
 
+#Summary
 summary(mymodel)
 
-#summary
-summary(model)
-
-#plot
+#Plot
 plot(mymodel,data=df, Rent~Size)
 
 pred=predict(mymodel,df)
