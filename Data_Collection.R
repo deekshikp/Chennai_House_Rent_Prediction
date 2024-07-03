5b2b39162ed63cb728c68f51ffa1da9eeed2eaa3
Dhee <150270603+heydhee@users.noreply.github.com>
2024-07-03 06:26:14 +0530

    Initial Commit

--- Data_Collection.R
+++ Data_Collection.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 
 #Filtering the dataset with values containing only from Chennai
+
 Rent.data<- data.frame(read.csv("House_Rent_Dataset.csv"))
 View(Rent.data)
 Chennai_rent <- Rent.data %>% dplyr::filter(City=="Chennai")
